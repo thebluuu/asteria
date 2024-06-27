a1abf860e0054c5d25adb2375c5c788d01da0269
thebluuu <154030933+thebluuu@users.noreply.github.com>
2024-06-27 07:38:06 +0100

    Update ClientPlayerInteractionManagerMixin.java

--- 1.20.2/src/main/java/net/caffeinemc/phosphor/mixin/ClientPlayerInteractionManagerMixin.java
+++ 1.20.2/src/main/java/net/caffeinemc/phosphor/mixin/ClientPlayerInteractionManagerMixin.java
@@ -15,20 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public abstract class ClientPlayerInteractionManagerMixin {
-    @Inject(at = @At("HEAD"), method = "getReachDistance()F", cancellable = true)
-    private void onGetReachDistance(CallbackInfoReturnable<Float> cir) {
-        ReachModule reachModule = Phosphor.moduleManager().getModule(ReachModule.class);
-        if (AsteriaMenu.isClientEnabled() && reachModule != null && reachModule.isEnabled() && reachModule.blockReachToggle.isEnabled()) {
-            cir.setReturnValue(reachModule.blockReach.getFValue());
-        }
-    }
-
-    @Inject(at = @At("HEAD"), method = "hasExtendedReach()Z", cancellable = true)
-    private void hasExtendedReach(CallbackInfoReturnable<Boolean> cir) {
-        if (AsteriaMenu.isClientEnabled() && Phosphor.moduleManager().isModuleEnabled(ReachModule.class)) {
-            cir.setReturnValue(false);
-        }
-    }
 
     @Inject(at = @At("HEAD"), method = "attackEntity")
     private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci) {
